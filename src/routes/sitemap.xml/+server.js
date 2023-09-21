c168a4604ccd11d6fb60382bb7266e8919a844e9
nomadWebDeveloper <kgjsmart1559@gmail.com>
2023-09-21 10:06:37 +0900

    users sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,5 +1,8 @@
-
+import axios from "axios";
+import { PUBLIC_BACKEND_URL, PUBLIC_DOMAIN_URL } from "$env/static/public";
 export async function GET() {
+  let emails = (await axios.get(`${PUBLIC_BACKEND_URL}/auth/users`)).data;
+  const userUrls = emails.map((/** @type {any} */ email)=>`${PUBLIC_DOMAIN_URL}/portfolio/${email}`);
   return new Response(
     `<?xml version="1.0" encoding="UTF-8"?>
     <urlset
@@ -9,7 +12,11 @@ export async function GET() {
                 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
     
-    
+    ${userUrls.map((/** @type {any} */ url)=>`
+      <url>
+        <loc>${url}</loc>
+      <url>
+    `).join("\n")}
     <url>
       <loc>https://www.dev-portfolio.kr/</loc>
       <lastmod>2023-09-20T15:01:34+00:00</lastmod>
