f2b3c1424e0f53a65911cd46b0383bd241117aac
nomadWebDeveloper <kgjsmart1559@gmail.com>
2023-09-26 17:26:02 +0900

    edit sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -10,7 +10,6 @@ export async function GET() {
           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
           xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
                 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-    <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
     
     ${userUrls.map((/** @type {any} */ url)=>`
       <url>
